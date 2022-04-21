5d118ebd2329a84240577e37cf5cf4f5b01f44e2
galileogenerobin <galileogenerobin@gmail.com>
2022-04-21 08:13:55 +0800

    Updated tictactoe.py
    1. Implemented player() function
    2. Implemented actions() function
    3. Implemented result() function

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,21 +23,59 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    
+    # Get the number of X's on the board
+    x_count = sum([row.count(X) for row in board])
+    # Get the number of O's on the board
+    o_count = sum([row.count[O] for row in board])
+
+    # If more Xs than Os, it's O's turn
+    if x_count > o_count:
+        return O
+    # Otherwise, it's X's turn
+    return X
+
+    # raise NotImplementedError
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    actions = set()
+    # Check for empty cells in the board
+    # Checking rows
+    for i in range(3):
+        # Checking columns
+        for j in range(3):
+            # If that cell is empty, add to the list of actions
+            if board[i][j] == EMPTY:
+                actions.add((i, j))
+
+    return actions
+
+    # raise NotImplementedError
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    # Get the coordinates of the action
+    i = action[0]
+    j = action[1]
+    # Raise error if move is invalid
+    if not board[i][j] == EMPTY:
+        raise ValueError('Move is invalid')
+
+    # Create a deepcopy of the board
+    board_copy = copy.deepcopy(board)
+    # Update the board state based on the action (by getting the current player and setting the value of the desired cell)
+    board_copy[i][j] = player(board)
+    # Return the new board state
+    return board_copy
+
+    # raise NotImplementedError
 
 
 def winner(board):
